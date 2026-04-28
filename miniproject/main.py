e3c8ca2722bfe95cff7ca42f018f946282b083cf
gems4175-star <gems4175@gmail.com>
2026-04-28 04:06:39 +0000

    good

--- miniproject/main.py
+++ miniproject/main.py
@@ -8,14 +8,14 @@ R...
 ..P.
 ....\
 """
-    checkmate(board1) # คาดหวังผลลัพธ์: Success (เพราะ Pawn ที่ 2,2 กิน K ที่ 1,1 ได้)
+    checkmate(board1) 
 
     print("\n--- Example 2 ---")
     board2 = """\
 ..
 .K\
 """
-    checkmate(board2) # คาดหวังผลลัพธ์: Fail (ไม่มีศัตรู)
+    checkmate(board2) 
 
     print("\n--- Custom Test (Error Handling) ---")
     board3 = """\
@@ -23,7 +23,7 @@ R...
 .K..
 ..P.
 """
-    checkmate(board3) # คาดหวังผลลัพธ์: Error (เพราะไม่ใช่จัตุรัส 3x4)
+    checkmate(board3) 
 
 if __name__ == "__main__":
     main()
